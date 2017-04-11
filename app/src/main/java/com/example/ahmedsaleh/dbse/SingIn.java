8b7c2470c560c9d09a22101b4a941ffbd486337f
MuhammadHamada <homshamada@gmail.com>
2017-04-11 18:27:22 +0200

    Sign_in with initial style

--- app/src/main/java/com/example/ahmedsaleh/dbse/SingIn.java
+++ app/src/main/java/com/example/ahmedsaleh/dbse/SingIn.java
@@ -2,6 +2,7 @@ package com.example.ahmedsaleh.dbse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class SingIn extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class SingIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
     }
 }
