835c88e982bbde8d7352149a90908ad561f6dcaa
MuhammadHamada <homshamada@gmail.com>
2017-04-12 14:18:06 +0200

    Add Facebook Button in signIn activity

--- app/src/main/java/com/example/ahmedsaleh/dbse/SingIn.java
+++ app/src/main/java/com/example/ahmedsaleh/dbse/SingIn.java
@@ -1,6 +1,8 @@
 package com.example.ahmedsaleh.dbse;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class SingIn extends AppCompatActivity {
 
@@ -17,6 +20,7 @@ public class SingIn extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button createaccount=(Button) findViewById(R.id.sign_in_button);
+        Button facebookPage =(Button) findViewById(R.id.Facebook_Button);
         createaccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,6 +28,16 @@ public class SingIn extends AppCompatActivity {
                 if(validate());
             }
         });
+        facebookPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent facebookIntent = openBbSE_FacebookPage(SingIn.this);
+                Intent chooser = Intent.createChooser(facebookIntent,"Open By");
+                startActivity(chooser);
+            }
+        });
+
     }
 
     @Override
@@ -62,4 +76,17 @@ public class SingIn extends AppCompatActivity {
         Intent i = new Intent(getApplicationContext(),SignUp.class);
         startActivity(i);
     }
+    public static Intent openBbSE_FacebookPage(Context context) {
+            try {
+                context.getPackageManager()
+                        .getPackageInfo("com.facebook.katana", 0);
+                return new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("fb://page/1220130878052358"));
+            } catch (Exception e){
+
+                return new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.facebook.com/pg/DatabaseSE.CO"));
+            }
+    }
+
 }
