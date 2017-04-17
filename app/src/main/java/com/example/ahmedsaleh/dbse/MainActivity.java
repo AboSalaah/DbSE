6b2cde0af31d57543f5aab11f5f92c92aef64528
Ahmed Saleh <ahmed_saleh102010@yahoo.com>
2017-04-17 18:21:22 +0200

    Add expandable list in each fragment

--- app/src/main/java/com/example/ahmedsaleh/dbse/MainActivity.java
+++ app/src/main/java/com/example/ahmedsaleh/dbse/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.ahmedsaleh.dbse;
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,14 +17,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-     TabLayout tabs;
+     private TabLayout tabs;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_universities);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         tabs = (TabLayout) findViewById(R.id.tabLayout);
@@ -32,8 +39,39 @@ public class MainActivity extends AppCompatActivity {
         // Set the adapter onto the view pager
         viewPager.setAdapter(adapter);
         tabs.setupWithViewPager(viewPager);
+      // setdatafornaivgationheader(); should be called when user sign in to update the circle in the navigation drawer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
+
+    /*void setdatafornaivgationheader()
+    {
+        NavigationView navigationView=(NavigationView)findViewById(R.id.navigationview);
+       View view= navigationView.getHeaderView(0);
+         RelativeLayout relativeLayout=(RelativeLayout)view;
+        TextView textView=(TextView)relativeLayout.findViewById(R.id.textincircle);
+        if(textView!=null)
+        {
+        GradientDrawable backgroundGradient = (GradientDrawable)textView.getBackground();
+        backgroundGradient.setColor(Color.BLUE);}
+    }
+    */
 
 
 }
