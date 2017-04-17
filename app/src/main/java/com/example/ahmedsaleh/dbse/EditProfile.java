99511fd24c8315b1a857f327f8f800f87146836f
MuhammadHamada <homshamada@gmail.com>
2017-04-18 01:15:24 +0200

    Edit tool bar

---
+++ app/src/main/java/com/example/ahmedsaleh/dbse/EditProfile.java
@@ -0,0 +1,16 @@
+package com.example.ahmedsaleh.dbse;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hamada on 4/18/2017.
+ */
+
+public class EditProfile extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
--- app/src/main/java/com/example/ahmedsaleh/dbse/MainActivity.java
+++ app/src/main/java/com/example/ahmedsaleh/dbse/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.ahmedsaleh.dbse;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.DragEvent;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -20,16 +23,26 @@ import android.view.MenuItem;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-     private TabLayout tabs;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private TabLayout tabs;
     private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_universities);
 
-         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout) ;
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        // drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationview);
+        navigationView.setNavigationItemSelectedListener(this);
+
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         tabs = (TabLayout) findViewById(R.id.tabLayout);
         // Create an adapter that knows which fragment should be shown on each page
@@ -39,39 +52,48 @@ public class MainActivity extends AppCompatActivity {
         // Set the adapter onto the view pager
         viewPager.setAdapter(adapter);
         tabs.setupWithViewPager(viewPager);
-      // setdatafornaivgationheader(); should be called when user sign in to update the circle in the navigation drawer
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     }
 
-    /*void setdatafornaivgationheader()
-    {
-        NavigationView navigationView=(NavigationView)findViewById(R.id.navigationview);
-       View view= navigationView.getHeaderView(0);
-         RelativeLayout relativeLayout=(RelativeLayout)view;
-        TextView textView=(TextView)relativeLayout.findViewById(R.id.textincircle);
-        if(textView!=null)
-        {
-        GradientDrawable backgroundGradient = (GradientDrawable)textView.getBackground();
-        backgroundGradient.setColor(Color.BLUE);}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.drawer_menu, menu);
+        return true;
     }
-    */
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if(id == R.id.Edit_profile){
+//            moveToEditProfileActiviy();  // ERROR !!!!!!!!!!!!!!!
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void moveToEditProfileActiviy() {
+        Intent i = new Intent(getApplicationContext(),EditProfile.class);
+        startActivity(i);
+    }
 
 }
